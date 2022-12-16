803a14f982e2a7a3fc32dca26d141838b4d77caa
siti2207111384 <120367414+siti2207111384@users.noreply.github.com>
2022-12-16 22:19:30 +0700

    Tugas 1 - Siti Rahmah

---
+++ Tugas 1/Program (1).cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Anda adalah agen rahasia yang bertugas mendapatkan data dari server ....");
+Console.WriteLine("Akses ke server membutuhkan password yang tidak diketahui....");
+Console.WriteLine("Saya Siti Rahmah - Teknik Informatika A");
+Console.Write("Universitas Riau");
